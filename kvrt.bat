f75987c5beb73f01f1d308cb5d91bcbd975d1712
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-01-31 22:08:36 -0500

    updated kvrt run script

--- kvrt.bat
+++ kvrt.bat
@@ -1,2 +1,4 @@
-bin\wget.exe
-pause
+@echo off
+set kvrtlog="%userprofile%\desktop\stinger\"
+if not exist %kvrtlog% md %kvrtlog%
+kvrt.exe -d "%kvrtlog%" -accepteula -adinsilent -silent -processlevel 2 -dontcryptsupportinfo
