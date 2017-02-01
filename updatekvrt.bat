25356ba0957e17a71c6e4eefeca6809775c5edd8
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-01-31 22:02:25 -0500

    added kvrt updater

--- updatekvrt.bat
+++ updatekvrt.bat
@@ -1,5 +1,5 @@
 @echo off
 REM -N If the local file is newer, the remote file will not be re-fetched http://www.editcorp.com/personal/lars_appel/wget/wget_5.html#SEC23
-bin\wget.exe -N http://downloadcenter.mcafee.com/products/mcafee-avert/stinger/stinger32.exe
-echo stinger downloaded on %date% > stinger.txt
+bin\wget.exe -N http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe
+echo KVRT downloaded on %date% > kvrt.txt
 pause
