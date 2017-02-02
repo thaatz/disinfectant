0d469bcde44198f1e7cc166180a4eda2c448ee5b
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-02-01 23:57:17 -0500

    added rootkit detection in stinger script

--- stinger.bat
+++ stinger.bat
@@ -9,6 +9,9 @@ REM --silent      : initiate a silent scan
 REM --program     : report applications
 REM --repair      : repair object if a virus is found
 REM --reportpath= : save log file to specified directory
+REM --rptall      : log all files
+REM --rootkit     : undocumented, but this enables scanning for rootkits
 REM --scanpath=   : scan specified directories (--scanpath=c:\)
 REM --epo         : run without the real protect component https://www.mcafee.com/us/downloads/free-tools/how-to-use-stinger.aspx
-stinger32.exe --go --silent --program --repair --reportpath=%stingerlog% --epo
+stinger32.exe --go --silent --program --repair --reportpath=%stingerlog% --rootkit --epo
+REM --scanpath=c:\ 
